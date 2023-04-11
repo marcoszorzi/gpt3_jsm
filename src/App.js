37797bdf919d53b16427ae1c6cda5806a92074e8
Marcos Zorzi Rosa <marcoszorzi@gmail.com>
2023-04-10 20:32:20 -0400

    Folder and files structucture

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 //rafce
 import React from 'react'
+import{ CTA, Brand, Navbar} from './components';
+import {Footer, Blog, Possibility, Features, WhatGPT3, Header} from './containers';
 
 const App = () => {
   return (
-    <div>
-        <h1>GPT-3</h1>
+    <div className='App'>
+        <div className='gradient__bg'>
+            <Navbar/>
+            <Header/>
+        </div>
+        <Brand/>
+        <WhatGPT3/>
+        <Features/>
+        <Possibility/>
+        <CTA/>
+        <Blog/>
+        <Footer/>
     </div>
   )
 }
--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './artible.css';
+import './article.css';
 
 const Article = () => {
   return (
