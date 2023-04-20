31a7f0a013a6727f3509bd4e8bb41245ec9c8f75
Marcos Zorzi Rosa <marcoszorzi@gmail.com>
2023-04-20 17:04:11 -0400

    Possibility styles done

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -4,13 +4,13 @@ import possibilityImage from '../../assets/possibility.png';
 
 const Possibility = () => {
   return (
-    <div className='gpt__possibility section__padding' id='possibility'>
+    <div className='gpt3__possibility section__padding' id='possibility'>
       <div className='gpt3__possibility-image'>
         <img src={possibilityImage} alt='possibility'/>
       </div>
       <div className='gpt3__possibility-content'>
         <h4>Request Early Access to Get Started</h4>
-        <h1 className='gradient-text'>The possibilities are beyond our imagination</h1>
+        <h1 className='gradient__text'>The possibilities are beyond our imagination</h1>
         <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
         <h4>Request Early Access to Get Started</h4>
       </div>
