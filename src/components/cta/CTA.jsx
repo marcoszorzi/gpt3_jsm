33605f7d07b5f7026eeadfedb8de9c21728987c7
Marcos Zorzi Rosa <marcoszorzi@gmail.com>
2023-04-28 16:20:39 -0400

    CTA structure

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -4,13 +4,15 @@ import './cta.css';
 const CTA = () => {
   return (
     <div className='gpt3__cta'>
-      
+      <div className='gpt3__cta-content'>
+        <p>Request Early Access to Get Started</p>
+        <h3>Register today & start exploring the endless possiblities.</h3>
+      </div>
+      <div className='gpt3__cta-button'>
+        <button type='button'>Get Started</button>
+      </div>
     </div>
   )
 }
 
 export default CTA
-
-/*Video stopped at: 2:24:20
-https://www.youtube.com/watch?v=F627pKNUCVQ&t=499s
- */
